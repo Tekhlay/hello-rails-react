c887a284d283958091f309626cef92d250eeae8f
Teklay <teklay@gmail.com>
2023-06-07 07:47:04 +0300

    Add message api end point controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
-  root 'root/index'
+  namespace :api do
+    namespace :v1 do
+      get 'messages/index'
+    end
+  end
+
+  root 'root#index'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+
   # Defines the root path route ("/")
+
+
+
   # root "articles#index"
+
 end
+
