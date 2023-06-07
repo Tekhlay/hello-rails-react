dbc4175cdad8a010b25c0c74e5d596284e71e1d7
Teklay <teklay@gmail.com>
2023-06-07 09:20:17 +0300

    Create table message and insert data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
+
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+
 #
+
 # Examples:
+
 #
+
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+Message.create(text: 'Hello, World!')
+
+Message.create(text: 'Goodbye, World!')
+
+Message.create(text: 'Hello, World again!')
+
+Message.create(text: 'Goodbye, World again!')
+
+Message.create(text: 'Hello, World again and again!')
+
