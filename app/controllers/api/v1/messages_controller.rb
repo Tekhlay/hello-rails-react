df7aea09ad7f95d407078771921c37fdc3475dc6
Teklay <teklay@gmail.com>
2023-06-07 09:27:41 +0300

    generate random message

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::MessagesController < ApplicationController
 
-  def index; end
+  def index
+
+    random_message = Message.order('RANDOM()').first
+
+    render json: { message: random_message.text }
+
+  end
 
 end
 
