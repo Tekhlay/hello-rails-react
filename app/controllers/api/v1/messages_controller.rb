f42b21d0db901af7da41ec7c055087428ea3a804
Teklay <teklay@gmail.com>
2023-06-08 04:46:44 +0300

    Fix api endpoit

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
     random_message = Message.order('RANDOM()').first
-
     render json: { message: random_message.text }
   end
 end
--- app/javascript/Redux/Greeting/greetingReducer.js
+++ app/javascript/Redux/Greeting/greetingReducer.js
@@ -25,7 +25,7 @@ const initialState = {
 export function getRandomGreeting() {
   return async function(dispatch) {
     try {
-      const response = await axios.get('/api/v1');
+      const response = await axios.get('/api/v1/messages');
       dispatch(getRandomGreetingSuccess(response.data.message));
     } catch (error) {
       dispatch(getRandomGreetingFailure(error));
